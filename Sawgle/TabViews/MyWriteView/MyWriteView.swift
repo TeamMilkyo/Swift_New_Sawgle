9672456c44bc44d924879917cf978e3e35dca795
dp221125 <dp221125@naver.com>
2019-07-09 10:22:22 +0900

    [Close #3] My PageView Redesign

--- Sawgle/TabViews/MyWriteView/MyWriteView.swift
+++ Sawgle/TabViews/MyWriteView/MyWriteView.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class MyWriteView: UIView {
+    let contentView: UIView = {
+        UIView()
+    }()
+
     let nameLabel: UILabel = {
         let nameLael = UILabel()
-        nameLael.font = UIFont().mainFont(displaySize: 35)
-        nameLael.textColor = UIColor.black
+        nameLael.font = UIFont().mainFont(displaySize: 25)
+        nameLael.textColor = UIColor(named: "brownishGray")
         nameLael.sizeToFit()
         nameLael.baselineAdjustment = .alignCenters
         return nameLael
@@ -34,7 +38,7 @@ class MyWriteView: UIView {
 
     let rightStackTitle: UILabel = {
         let rightStackTitle = UILabel()
-        rightStackTitle.font = UIFont().mainFont(displaySize: 15)
+        rightStackTitle.font = UIFont(name: "S-CoreDream-3Light", size: 10)
         rightStackTitle.textColor = UIColor(named: "PaleBrown")
         rightStackTitle.text = "마지막 쓴 글"
         rightStackTitle.textAlignment = .right
@@ -43,8 +47,8 @@ class MyWriteView: UIView {
 
     let dateLabel: UILabel = {
         let dateLabel = UILabel()
-        dateLabel.font = UIFont().mainFont(displaySize: 15)
-        dateLabel.textColor = UIColor.black
+        dateLabel.font = UIFont(name: "S-CoreDream-3Light", size: 13)
+        dateLabel.textColor = UIColor(named: "brownishGray")
         dateLabel.textAlignment = .right
         return dateLabel
     }()
@@ -70,9 +74,22 @@ class MyWriteView: UIView {
     let goBoardButton: UIButton = {
         let goBoardButton = UIButton()
         goBoardButton.setTitle("게시판에서 보기", for: .normal)
-        goBoardButton.setTitleColor(UIColor(named: "PalePeach"), for: .normal)
-        goBoardButton.titleLabel?.font = UIFont(name: "S-CoreDream-5Medium", size: 15)
+        goBoardButton.setTitleColor(UIColor(named: "NewBrown"), for: .normal)
+        goBoardButton.titleLabel?.font = UIFont(name: "S-CoreDream-3Light", size: 10)
         return goBoardButton
+    }()
+
+    let commentTitleLabel: UILabel = {
+        let commentTitleLabel = UILabel()
+        commentTitleLabel.font = UIFont(name: "S-CoreDream-3Light", size: 13)
+        commentTitleLabel.text = "실시간 댓글"
+        return commentTitleLabel
+    }()
+
+    let commentTitleLine: UIView = {
+        let commentTitleLine = UIView()
+        commentTitleLine.backgroundColor = .white
+        return commentTitleLine
     }()
 
     let mySubscribePostView: UICollectionView = {
@@ -89,7 +106,7 @@ class MyWriteView: UIView {
         self.nameLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             nameLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 10),
-            nameLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            nameLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 32),
             nameLabel.heightAnchor.constraint(equalToConstant: 40),
         ])
     }
@@ -98,8 +115,8 @@ class MyWriteView: UIView {
         self.nameImageView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             nameImageView.bottomAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 0),
-            nameImageView.leadingAnchor.constraint(equalTo: nameLabel.trailingAnchor, constant: 0),
-            nameImageView.heightAnchor.constraint(equalTo: nameLabel.heightAnchor, multiplier: 0.6),
+            nameImageView.leadingAnchor.constraint(equalTo: nameLabel.trailingAnchor, constant: 5),
+            nameImageView.heightAnchor.constraint(equalTo: nameLabel.heightAnchor, multiplier: 0.4),
             nameImageView.widthAnchor.constraint(equalTo: nameImageView.heightAnchor),
         ])
     }
@@ -107,8 +124,8 @@ class MyWriteView: UIView {
     func makeRightStackViewConstraint() {
         self.rightStackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            rightStackView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 10),
-            rightStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            rightStackView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 15),
+            rightStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -32),
             rightStackView.heightAnchor.constraint(equalTo: nameLabel.heightAnchor),
         ])
     }
@@ -116,10 +133,10 @@ class MyWriteView: UIView {
     func makeMyWriteViewConstraint() {
         self.myWriteSectionView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            myWriteSectionView.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 16),
+            myWriteSectionView.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 5),
             myWriteSectionView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.15),
-            myWriteSectionView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            myWriteSectionView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            myWriteSectionView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32),
+            myWriteSectionView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -32),
         ])
     }
 
@@ -128,8 +145,26 @@ class MyWriteView: UIView {
         NSLayoutConstraint.activate([
             myCommentSectionView.topAnchor.constraint(equalTo: myWriteSectionView.bottomAnchor, constant: 10),
             myCommentSectionView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.15),
-            myCommentSectionView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            myCommentSectionView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.5, constant: -21),
+            myCommentSectionView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32),
+            myCommentSectionView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.5, constant: -37),
+        ])
+    }
+
+    func addCommentTitleLabelConstraint() {
+        self.commentTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            commentTitleLabel.topAnchor.constraint(equalTo: myCommentSectionView.bottomAnchor, constant: 16),
+            commentTitleLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 32),
+        ])
+    }
+
+    func addCommentTitleLineConstraint() {
+        self.commentTitleLine.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            commentTitleLine.topAnchor.constraint(equalTo: commentTitleLabel.bottomAnchor, constant: 5),
+            commentTitleLine.heightAnchor.constraint(equalToConstant: 2),
+            commentTitleLine.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            commentTitleLine.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16),
         ])
     }
 
@@ -139,15 +174,15 @@ class MyWriteView: UIView {
             myLikePostSectionView.topAnchor.constraint(equalTo: myWriteSectionView.bottomAnchor, constant: 10),
             myLikePostSectionView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.15),
             myLikePostSectionView.leadingAnchor.constraint(equalTo: myCommentSectionView.trailingAnchor, constant: 10),
-            myLikePostSectionView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.5, constant: -21),
+            myLikePostSectionView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.5, constant: -37),
         ])
     }
 
     func makeMySubscribePostViewConstraint() {
         self.mySubscribePostView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            mySubscribePostView.topAnchor.constraint(equalTo: myLikePostSectionView.bottomAnchor, constant: 10),
-            mySubscribePostView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.24),
+            mySubscribePostView.topAnchor.constraint(equalTo: myLikePostSectionView.bottomAnchor, constant: 50),
+            mySubscribePostView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.32),
             mySubscribePostView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
             mySubscribePostView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
         ])
@@ -167,6 +202,8 @@ class MyWriteView: UIView {
         addSubview(self.rightStackView)
         addSubview(self.myWriteSectionView)
         addSubview(self.myCommentSectionView)
+        addSubview(self.commentTitleLabel)
+        addSubview(self.commentTitleLine)
         addSubview(self.myLikePostSectionView)
         addSubview(self.mySubscribePostView)
         self.myWriteSectionView.addSubview(self.goBoardButton)
@@ -178,6 +215,8 @@ class MyWriteView: UIView {
         self.makeRightStackViewConstraint()
         self.makeMyWriteViewConstraint()
         self.makeMyCommentViewConstraint()
+        self.addCommentTitleLabelConstraint()
+        self.addCommentTitleLineConstraint()
         self.makeMyLikePostViewConstraint()
         self.makeMySubscribePostViewConstraint()
         self.goBoardButtonConstraint()
